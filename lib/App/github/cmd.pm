dd0be9f0019a94d521f9b7cbca498ce493a8e635
perlancar (on pc-office) <perlancar@gmail.com>
2018-10-09 17:01:13 +0700

    Release v0.001

--- lib/App/github/cmd.pm
+++ lib/App/github/cmd.pm
@@ -17,33 +17,183 @@ $SPEC{':package'} = {
 
 our %args_common = (
     login => {
+        schema => 'str*',
     },
     pass => {
+        schema => 'str*',
     },
     access_token => {
+        schema => 'str*',
     },
 );
 our %argsrels_common = (
     req_all => [qw/login pass/],
     req_one => [qw/login access_token/],
 );
+our %arg0_user = (
+    user => {
+        schema => 'str*',
+        req => 1,
+        pos => 0,
+    },
+);
+our %argopt0_user = (
+    user => {
+        schema => 'str*',
+        pos => 0,
+    },
+);
+our %argopt_user = (
+    user => {
+        schema => 'str*',
+    },
+);
+our %arg0_repo = (
+    repo => {
+        schema => 'str*',
+        req => 1,
+        pos => 0,
+    },
+);
+our %argopt_detail = (
+    detail => {
+        schema => 'bool*',
+        cmdline_aliases => {l=>{}},
+    },
+);
+
+sub _init {
+    my $args = shift;
+    state $state = {};
+
+    unless ($state->{_github}) {
+        require Net::GitHub;
+        my %ngargs;
+        if ($args->{access_token}) {
+            $ngargs{access_token} = $args->{access_token};
+        } else {
+            $ngargs{login} = $args->{login};
+            $ngargs{pass}  = $args->{pass};
+        }
+        $state->{github} = Net::GitHub->new(%ngargs);
+    }
+    $state;
+}
+
+$SPEC{get_user} = {
+    v => 1.1,
+    summary => 'Get information about a user',
+    args => {
+        %args_common,
+        %argopt0_user,
+    },
+};
+sub get_user {
+    my %args = @_;
+    my $state = _init(\%args);
+    my $github = $state->{github};
+
+    my $user = $github->user->show($args{user});
+    [200, "OK", $user];
+}
 
 $SPEC{get_repo} = {
     v => 1.1,
+    summary => 'Get information about a repository',
+    args => {
+        %args_common,
+        %argopt_user,
+        %arg0_repo,
+    },
+};
+sub get_repo {
+    my %args = @_;
+    my $state = _init(\%args);
+    my $github = $state->{github};
+
+    my $repo = $github->repos->get($args{user} // $args{login}, $args{repo});
+    [200, "OK", $repo];
+}
+
+$SPEC{repo_exists} = {
+    v => 1.1,
+    summary => 'Check whether a repository exists',
+    args => {
+        %args_common,
+        %argopt_user,
+        %arg0_repo,
+    },
 };
 sub repo_exists {
+    my %args = @_;
+    my $state = _init(\%args);
+    my $github = $state->{github};
+
+    my $repo;
+    eval {
+        $repo = $github->repos->get($args{user} // $args{login}, $args{repo});
+    };
+    my $err = $@;
+    my $exists = $err && $err =~ /Not Found/ ? 0 : 1;
+    [200, "OK", $exists];
+}
+
+$SPEC{list_repos} = {
+    v => 1.1,
+    summary => "List user's repositories",
+    args => {
+        %args_common,
+        %argopt_detail,
+        start => {
+            schema => 'nonnegint*',
+            default => 0,
+        },
+    },
+};
+sub list_repos {
+    my %args = @_;
+    my $state = _init(\%args);
+    my $github = $state->{github};
+
+    my @repos = $github->repos->list($args{start});
+    unless ($args{detail}) {
+        @repos = map { $_->{name} } @repos;
+    }
+    [200, "OK", \@repos];
 }
 
 $SPEC{create_repo} = {
     v => 1.1,
+    summary => 'Create a repository',
+    args => {
+        %args_common,
+        %arg0_repo,
+        description => {
+            schema => 'str*',
+        },
+        homepage => {
+            schema => 'url*',
+        },
+    },
 };
 sub create_repo {
+    my %args = @_;
+    my $state = _init(\%args);
+    my $github = $state->{github};
+
+    $github->repos->create(
+        name => $args{repo},
+        description => $args{description} // '(No description)',
+        homepage    => $args{homepage} ? "$args{homepage}" : 'https://github.com',
+    );
+    [200, "OK"];
 }
 
 $SPEC{delete_repo} = {
     v => 1.1,
 };
 sub delete_repo {
+    [501, "Not yet implemented"];
 }
 
 
--- lib/Perinci/CmdLine/github/cmd.pm
+++ lib/Perinci/CmdLine/github/cmd.pm
@@ -23,12 +23,50 @@ sub hook_before_parse_argv {
         $self->_read_config($r) unless $r->{config};
         last if defined($r->{config}{GLOBAL}{login}) &&
             defined($r->{config}{GLOBAL}{pass}) ||
-            defined($r->{config}{GLOBAL}{token});
+            defined($r->{config}{GLOBAL}{access_token});
         log_trace "User does not have defined login+pass or token in ".
             "configuration, running setup ...";
-        # XXX RUN SETUP
-    }
+        require Term::ReadKey;
+        my ($login, $pass);
+        while (1) {
+            print "Setting up github-cmd. Please enter GitHub login: ";
+            chomp($login = <STDIN>);
+            last if $login =~ /\A\w+\z/;
+        }
+        while (1) {
+            print "Please enter GitHub password: ";
+            Term::ReadKey::ReadMode('noecho');
+            chomp($pass = <STDIN>);
+            Term::ReadKey::ReadMode('normal');
+            print "\n";
+            last if length $login;
+        }
+        require PERLANCAR::File::HomeDir;
+        my $path = PERLANCAR::File::HomeDir::get_my_home_dir() .
+            "/github-cmd.conf";
+        require Config::IOD;
+        my $iod = Config::IOD->new;
+        require Config::IOD::Document;
+        my $doc = (-f $path) ? $iod->read_file($path) :
+            Config::IOD::Document->new;
+        $doc->insert_section({ignore=>1}, 'GLOBAL');
+        $doc->insert_key({replace=>1}, 'GLOBAL', 'login', $login);
+        $doc->insert_key({replace=>1}, 'GLOBAL', 'pass', $pass);
+        open my $fh, ">", $path or die "Can't write config '$path': $!";
+        print $fh $doc->as_string;
+        close $fh;
+
+        # early exit
+        print "Setup done, please run 'github-cmd' again.\n";
+        exit 0;
+    } # RUN_SETUP
 }
 
 1;
 # ABSTRACT: Subclass for github-cmd
+
+=head1 DESCRIPTION
+
+This subclass adds a hook at the C<before_parse_argv> phase to run setup
+(prompting login+pass, then writing configuration file) if login+pass are not
+found in configuration.
